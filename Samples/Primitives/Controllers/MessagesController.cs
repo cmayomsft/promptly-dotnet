a536df3311d90996c8f80b35d58ef56e1df2d8a2
Chris Mayo <cmayo@microsoft.com>
2018-02-14 19:59:17 -0800

    Simple PPP echo bot.

--- Samples/Primitives/Controllers/MessagesController.cs
+++ Samples/Primitives/Controllers/MessagesController.cs
@@ -13,15 +13,29 @@ namespace Primitives.Controllers
 
         protected override Task OnReceiveActivity(IBotContext context)
         {
-            if (context.Request.Type == ActivityTypes.Message)
+            if ((context.Request.Type == ActivityTypes.Message) && (context.Request.AsMessageActivity().Text.Length > 0))
             {
-                var message = context.Request.AsMessageActivity().Text;
-
-                var turnNumber = (int)(context.State.Conversation["turnNumber"] ?? 1);
-
-                context.Reply($"Turn: {turnNumber} - You said '{ message }'!");
-
-                context.State.Conversation["turnNumber"] = ++turnNumber;
+                // If bot doesn't have state it needs, prompt for it.
+                if (context.State.User["name"] == null)
+                {
+                    // On the first turn, prompt and update state that conversation is in a prompt.
+                    if (context.State.Conversation["prompt"] != "name")
+                    {
+                        context.State.Conversation["prompt"] = "name";
+                        context.Reply("What is your name?");
+                        // On the subsequent turn, update state with reply and update state that prompt has completed. 
+                    }
+                    else
+                    {
+                        context.State.Conversation["prompt"] = "";
+                        context.State.User["name"] = context.Request.AsMessageActivity().Text;
+                        context.Reply($"Great, I'll call you ${ context.State.User["name"] }!");
+                    }
+                }
+                else
+                {
+                    context.Reply($"{ context.State.User["name"]} said: { context.Request.AsMessageActivity().Text }");
+                }
             }
 
             return Task.CompletedTask;
