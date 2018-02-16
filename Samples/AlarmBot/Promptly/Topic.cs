caed2c3dc77271199de4729565dc609a7c0b3d54
Chris Mayo <cmayo@microsoft.com>
2018-02-15 19:04:55 -0800

    First pass at Topic.

--- Samples/AlarmBot/Promptly/Topic.cs
+++ Samples/AlarmBot/Promptly/Topic.cs
@@ -10,16 +10,16 @@ namespace Promptly
     //  V = Interface for the resulting value for when the Topic completes successfully.
     //      Optional for cases where the Topic doesn't need to return a value. 
     // TODO: Make V optional generic type.
-    public abstract class Topic<S, V>
+    public abstract class Topic<TState, TValue>
     {
-        private S state;
+        private TState state;
 
-        public Topic(S state)
+        public Topic(TState state)
         {
             this.State = state;
         }
 
-        public S State { get => state; set => state = value; }
+        public TState State { get => state; set => state = value; }
 
         // onReceive - Called on each turn when Topic is the active topic of conversation.
         protected abstract Task OnReceiveActivity(IBotContext context);
@@ -27,7 +27,7 @@ namespace Promptly
         // onSuccess - Function to call when the Topic completes successfully, passing the
         //  resulting value of the Topic.
         protected Func<IBotContext, V, Task> success;
-        public Topic<S, V> OnSuccess(Func<IBotContext, V, Task> success)
+        public Topic<TState, V> OnSuccess(Func<IBotContext, V, Task> success)
         {
             this.success = success;
             return this;
@@ -36,7 +36,7 @@ namespace Promptly
         // onFailure - Function to call when the Topic ends unsuccessfully, passing the reason
         //  why the Topic failed. 
         protected Func<IBotContext, string, Task> failure;
-        public Topic<S, V> OnFailure(Func<IBotContext, string, Task> failure)
+        public Topic<TState, V> OnFailure(Func<IBotContext, string, Task> failure)
         {
             this.failure = failure;
             return this;
