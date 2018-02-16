e5c5a94f54213426b2700ec83f84e80c4bb3f791
Chris Mayo <cmayo@microsoft.com>
2018-02-15 20:25:20 -0800

    Interim checkin, unfinished and untested.

---
+++ Samples/AlarmBot/Promptly/Prompt.cs
@@ -0,0 +1,43 @@
+﻿using AlarmBot.Promptly.Validator;
+using Microsoft.Bot.Builder;
+using Promptly;
+using System;
+using System.Threading.Tasks;
+
+namespace AlarmBot.Promptly
+{
+    public class Prompt<TValue> : Topic<TValue>
+    {
+        public int Turns { get; set; } = 0;
+
+        private Func<IBotContext, string, Task> _OnPrompt;
+        public Topic OnPrompt(Func<IBotContext, string, Task> onPrompt)
+        {
+            _OnPrompt = onPrompt;
+            return this;
+        }
+
+        private int _MaxTurns = 2;
+        public Topic MaxTurns(int maxTurns)
+        {
+            _MaxTurns = maxTurns;
+            return this;
+        }
+
+        private Validator<TValue> _Validator;
+        public Topic Validator(Validator<TValue> validator)
+        {
+            _Validator = validator;
+            return this;
+        }
+
+        public override Task OnReceiveActivity(IBotContext context)
+        {
+            if (this.Turns == 0)
+            {
+                this.OnPrompt(context);
+                return
+            }
+        }
+    }
+}
--- Samples/AlarmBot/Promptly/Topic.cs
+++ Samples/AlarmBot/Promptly/Topic.cs
@@ -6,11 +6,8 @@ namespace Promptly
 {
     public abstract class Topic
     {
-        // onReceive - Called on each turn when Topic is the active topic of conversation.
-        protected abstract Task OnReceiveActivity(IBotContext context);
+        public abstract Task OnReceiveActivity(IBotContext context);
 
-        // onSuccess - Function to call when the Topic completes successfully, passing the
-        //  resulting value of the Topic.
         protected Func<IBotContext, Task> success;
         public Topic OnSuccess(Func<IBotContext, Task> success)
         {
@@ -18,8 +15,6 @@ namespace Promptly
             return this;
         }
 
-        // onFailure - Function to call when the Topic ends unsuccessfully, passing the reason
-        //  why the Topic failed. 
         protected Func<IBotContext, string, Task> failure;
         public Topic OnFailure(Func<IBotContext, string, Task> failure)
         {
@@ -30,8 +25,6 @@ namespace Promptly
 
     public abstract class Topic<TValue> : Topic
     {
-        // onSuccess - Function to call when the Topic completes successfully, passing the
-        //  resulting value of the Topic.
         protected new Func<IBotContext, TValue, Task> success;
         public Topic OnSuccess(Func<IBotContext, TValue, Task> success)
         {
