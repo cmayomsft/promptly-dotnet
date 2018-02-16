c4d6d6fcd393983013e465dec00375d3bf97e4d6
Chris Mayo <cmayo@microsoft.com>
2018-02-15 19:03:25 -0800

    Created simple sample for persisting topic objects.

---
+++ Samples/SimpleTopic/Controllers/MessagesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Threading.Tasks;
+using SimpleTopics.Topics;
+
+namespace SimpleTopics.Controllers
+{
+    [Route("api/[controller]")]
+    public class MessagesController : BotController
+    {
+        public MessagesController(Bot bot) : base(bot) { }
+
+        protected override Task OnReceiveActivity(IBotContext context)
+        {
+
+            if ((context.Request.Type == ActivityTypes.Message) && (context.Request.AsMessageActivity().Text.Length > 0))
+            {
+                var activeTopic = (SimpleTopic)context.State.Conversation["ActiveTopic"];
+
+                if (activeTopic == null)
+                {
+                    activeTopic = new SimpleTopic();
+                    context.State.Conversation["ActiveTopic"] = activeTopic;
+                }
+
+                activeTopic.OnReceiveActivity(context);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
