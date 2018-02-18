860ba3c35726d952f7251db762dae9de2f8f099f
Chris Mayo <cmayo@microsoft.com>
2018-02-18 13:31:11 -0800

    Moved lib classes to Promptly project.

---
+++ Libraries/PromptlyBot/Prompt.cs
@@ -0,0 +1,80 @@
+﻿using AlarmBot.Promptly.Validator;
+using Microsoft.Bot.Builder;
+using Promptly;
+using System;
+using System.Threading.Tasks;
+
+namespace AlarmBot.Promptly
+{
+    public class Prompt<TValue> : Topic<TValue>
+    {
+        private int _turns = 0;
+        public int Turns
+        {
+            get
+            {
+                return _turns;
+            }
+        }
+
+        private Func<IBotContext, string, Task> _onPrompt;
+        public Topic OnPrompt(Func<IBotContext, string, Task> onPrompt)
+        {
+            _onPrompt = onPrompt;
+            return this;
+        }
+
+        private int _maxTurns = 2;
+        public Topic MaxTurns(int maxTurns)
+        {
+            _maxTurns = maxTurns;
+            return this;
+        }
+
+        private Validator<TValue> _validator;
+        public Topic Validator(Validator<TValue> validator)
+        {
+            _validator = validator;
+            return this;
+        }
+
+        public override Task OnReceiveActivity(IBotContext context)
+        {
+            // If this is the initial turn (turn 0), send the initial prompt.
+            if (_turns == 0)
+            {
+                _onPrompt(context, null);
+                return Task.CompletedTask;
+            }
+
+            // For all subsequent turns...
+
+            // Validate the message/reply from the last turn.
+            var validationResult = _validator.Validate(context);
+
+            // If the message/reply wasn't a valid response to the prompt...
+            if (validationResult.Reason != null)
+            {
+                // Increase the turn count.
+                _turns += 1;
+
+                // If max turns has been reached, the prompt has failed with too many attempts.
+                if (_turns == _maxTurns)
+                {
+                    validationResult.Reason = "toomanyattempts";
+
+                    _onFailure(context, validationResult.Reason);
+                    return Task.CompletedTask;
+                }
+
+                // Re-prompt, providing the validation reason from last turn.
+                _onPrompt(context, validationResult.Reason);
+                return Task.CompletedTask;
+            }
+
+            // Prompt was successful, so pass value (result) of the Prompt.
+            _onSuccess(context, validationResult.Value);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Libraries/PromptlyBot/Topic.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Bot.Builder;
+using System;
+using System.Threading.Tasks;
+
+namespace Promptly
+{
+    public abstract class Topic
+    {
+        public abstract Task OnReceiveActivity(IBotContext context);
+
+        protected Func<IBotContext, Task> _onSuccess;
+        public Topic OnSuccess(Func<IBotContext, Task> success)
+        {
+            _onSuccess = success;
+            return this;
+        }
+
+        protected Func<IBotContext, string, Task> _onFailure;
+        public Topic OnFailure(Func<IBotContext, string, Task> failure)
+        {
+            _onFailure = failure;
+            return this;
+        }
+    }
+
+    public abstract class Topic<TValue> : Topic
+    {
+        protected new Func<IBotContext, TValue, Task> _onSuccess;
+        public Topic OnSuccess(Func<IBotContext, TValue, Task> success)
+        {
+            _onSuccess = success;
+            return this;
+        }
+    }
+}
