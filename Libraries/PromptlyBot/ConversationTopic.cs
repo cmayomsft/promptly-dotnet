d9a6e9d1e0a396afdb063498dd2b64aae7a4a7bd
Chris Mayo <cmayo@microsoft.com>
2018-02-18 15:56:47 -0800

    Removed Topic/ConvTopic that don't return values to simplify. Will add back later.

--- Libraries/PromptlyBot/ConversationTopic.cs
+++ Libraries/PromptlyBot/ConversationTopic.cs
@@ -1,15 +1,5 @@
 ﻿namespace PromptlyBot
 {
-    public abstract class ConversationTopic : Topic
-    {
-        public Topic ActiveTopic { get; set; }
-
-        public bool HasActiveTopic => (ActiveTopic != null);
-
-        public void ClearActiveTopic() => ActiveTopic = null;
-    }
-
-
     public abstract class ConversationTopic<TValue> : Topic<TValue>
     {
         public Topic ActiveTopic { get; set; }
--- Libraries/PromptlyBot/Topic.cs
+++ Libraries/PromptlyBot/Topic.cs
@@ -4,31 +4,21 @@ using System.Threading.Tasks;
 
 namespace PromptlyBot
 {
-    public abstract class Topic
+    public abstract class Topic<TValue>
     {
         public abstract Task OnReceiveActivity(IBotContext context);
 
-        protected Action<IBotContext> _onSuccess;
-        public Topic OnSuccess(Action<IBotContext> success)
+        protected Action<IBotContext, TValue> _onSuccess;
+        public Topic<TValue> OnSuccess(Action<IBotContext, TValue> success)
         {
             _onSuccess = success;
             return this;
         }
 
         protected Action<IBotContext, string> _onFailure;
-        public Topic OnFailure(Action<IBotContext, string> failure)
+        public Topic<TValue> OnFailure(Action<IBotContext, string> failure)
         {
             _onFailure = failure;
-            return this;
-        }
-    }
-
-    public abstract class Topic<TValue> : Topic
-    {
-        protected new Action<IBotContext, TValue> _onSuccess;
-        public Topic<TValue> OnSuccess(Action<IBotContext, TValue> success)
-        {
-            _onSuccess = success;
             return this;
         }
     }
