4fc2b594b462a12117deb3eab3dc06b2563607ee
Chris Mayo <cmayo@microsoft.com>
2018-02-24 20:14:57 +0100

    Removed test classes no longer needed.

--- Samples/AlarmBot/Topics/RootTopic.cs
+++ Samples/AlarmBot/Topics/RootTopic.cs
@@ -9,54 +9,18 @@ namespace AlarmBot.Topics
 {
     public class RootTopic : TopicsRoot
     {
-        private const string SIMPLE_TOPIC = "simpleTopic";
-        private const string SIMPLE_VALUE_TOPIC = "simpleValueTopic";
         private const string ADD_ALARM_TOPIC = "addAlarmTopic";
 
         private const string USER_STATE_ALARMS = "Alarms";
 
         public RootTopic(IBotContext context) : base(context)
         {
+            // User state initialization should be done once in the welcome 
+            //  new user feature. Placing it here until that feature is added.
             if (context.State.User[USER_STATE_ALARMS] == null)
             {
                 context.State.User[USER_STATE_ALARMS] = new List<Alarm>();
             }
-
-            this.SubTopics.Add(SIMPLE_TOPIC, () =>
-                {
-                    return new SimpleTopic
-                    {
-                        OnSuccess = (ctx) =>
-                            {
-                                context.Reply($"SimpleTopic.OnSuccess()");
-                                this.ClearActiveTopic();
-                            },
-                        OnFailure = (ctx, reason) =>
-                            {
-                                this.ClearActiveTopic();
-                                context.Reply($"SimpleTopic.OnFailure() - { reason }");
-                            },
-
-                    };
-                });
-
-            this.SubTopics.Add(SIMPLE_VALUE_TOPIC, () =>
-            {
-                return new SimpleValueTopic
-                {
-                    OnSuccess = (ctx, value) =>
-                    {
-                        context.Reply($"SimpleValueTopic.OnSuccess() - { value }");
-                        this.ClearActiveTopic();
-                    },
-                    OnFailure = (ctx, reason) =>
-                    {
-                        this.ClearActiveTopic();
-                        context.Reply($"SimpleValueTopic.OnFailure() - { reason }");
-                    },
-
-                };
-            });
 
             this.SubTopics.Add(ADD_ALARM_TOPIC, () =>
             {
@@ -80,8 +44,6 @@ namespace AlarmBot.Topics
                     }
                 };
             });
-
-
         }
 
         public override Task OnReceiveActivity(IBotContext context)
@@ -89,45 +51,6 @@ namespace AlarmBot.Topics
             if ((context.Request.Type == ActivityTypes.Message) && (context.Request.AsMessageActivity().Text.Length > 0))
             {
                 var message = context.Request.AsMessageActivity();
-
-                if (message.Text.ToLowerInvariant() == "simple")
-                {
-                    this.SetActiveTopic(SIMPLE_TOPIC);
-                    this.ActiveTopic.OnReceiveActivity(context);
-                    return Task.CompletedTask;
-                }
-
-                if (message.Text.ToLowerInvariant() == "simple value")
-                {
-                    this.SetActiveTopic(SIMPLE_VALUE_TOPIC);
-                    this.ActiveTopic.OnReceiveActivity(context);
-                    return Task.CompletedTask;
-                }
-
-                /*if (message.Text.ToLowerInvariant() == "simple conversation topic")
-                {
-                    var simpleConversationTopic = new SimpleConversationTopic();
-                    simpleConversationTopic.OnSuccess = (ctx) => { };
-                    simpleConversationTopic.OnFailure = (ctx, reason) => { };
-
-                    this.ActiveTopic = simpleConversationTopic;
-
-                    simpleConversationTopic.OnReceiveActivity(context);
-                    return Task.CompletedTask;
-                }*/
-
-                /*if (message.Text.ToLowerInvariant() == "simple value conversation topic")
-                {
-                    var simpleValueConversationTopic = new SimpleValueConversationTopic();
-                    simpleValueConversationTopic.OnSuccess = (ctx, value) => { };
-                    simpleValueConversationTopic.OnFailure = (ctx, reason) => { };
-
-                    this.ActiveTopic = simpleValueConversationTopic;
-
-                    simpleValueConversationTopic.OnReceiveActivity(context);
-                    return Task.CompletedTask;
-                }*/
-
 
                 if (message.Text.ToLowerInvariant() == "add alarm")
                 {
