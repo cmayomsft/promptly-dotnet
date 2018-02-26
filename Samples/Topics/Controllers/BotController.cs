ecfbcc1888585e32f7f6397fc9e5811be7e68c6b
Chris Mayo <cmayo@microsoft.com>
2018-02-26 14:16:45 +0100

    First pass at topics demo.

---
+++ Samples/Topics/Controllers/BotController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Adapters;
+using Microsoft.Bot.Schema;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Topics.Controllers
+{
+    /// <summary>
+    /// Helper Bot Controller for ASP.NET Core
+    /// </summary>
+    public abstract class BotController : Controller
+    {
+        protected readonly BotFrameworkAdapter _adapter;
+
+        public BotController(BotFrameworkAdapter adapter)
+        {
+            this._adapter = adapter;
+        }
+
+        protected abstract Task OnReceiveActivity(IBotContext context);
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]Activity activity)
+        {
+            try
+            {
+                await _adapter.ProcessActivty(this.Request.Headers["Authorization"].FirstOrDefault(), activity, OnReceiveActivity);
+                return this.Ok();
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return this.Unauthorized();
+            }
+            catch (InvalidOperationException e)
+            {
+                return this.NotFound(e.Message);
+            }
+        }
+    }
+}
---
+++ Samples/Topics/Models/Alarm.cs
@@ -0,0 +1,8 @@
+﻿namespace Topics.Models
+{
+    public class Alarm
+    {
+        public string Title { get; set; }
+        public string Time { get; set; }
+    }
+}
