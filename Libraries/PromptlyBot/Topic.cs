126eccbd33442b45e7413a637c8e1976649c7b93
Chris Mayo <cmayo@microsoft.com>
2018-03-01 15:32:46 +0100

    Added code comments.

--- Libraries/PromptlyBot/Topic.cs
+++ Libraries/PromptlyBot/Topic.cs
@@ -39,7 +39,7 @@ namespace PromptlyBot
     }
 
     /// <summary>
-    /// Represents a conversation topic that doesn't return a value when it completes successfully.
+    /// Abstract class that represents a conversation topic that doesn't return a value when it completes successfully.
     /// </summary>
     /// <typeparam name="TState">
     /// The state of the conversation topic that will be persisted between conversation turns.
@@ -64,7 +64,7 @@ namespace PromptlyBot
         public TopicFluentInterface Set { get => _set; }
 
         /// <value>
-        /// Protected access to state for the fluent API interface class.
+        /// Protected access to state for the fluent API class.
         /// </value>
         protected TState _state;
         /// <value>
@@ -83,9 +83,9 @@ namespace PromptlyBot
         public abstract Task OnReceiveActivity(IBotContext context);
 
         private Action<IBotContext> _onSuccess;
-        /// <summary>
+        /// <value>
         /// See <see cref = "ITopic.OnSuccess"/> for more details. 
-        /// </summary>
+        /// </value>
         public Action<IBotContext> OnSuccess { get => _onSuccess; set => _onSuccess = value; }
 
         private Action<IBotContext, string> _onFailure;
@@ -93,22 +93,39 @@ namespace PromptlyBot
         /// See <see cref = "ITopic.OnFailure"/> for more details. 
         /// </summary>
         public Action<IBotContext, string> OnFailure { get => _onFailure; set => _onFailure = value; }
-
+        
+        /// <summary>
+        /// Internal class to define the fluent API for <see cref="Topic{TState}"/>.
+        /// </summary>
         public class TopicFluentInterface
         {
             private readonly Topic<TState> _topic;
 
+            /// <summary>
+            /// Establishes reference between Topic and this fluent API class.
+            /// </summary>
+            /// <param name="topic">The <see cref="Topic{TState}"/> referenced in the fluent API.</param>
             public TopicFluentInterface(Topic<TState> topic)
             {
                 this._topic = topic;
             }
 
+            /// <summary>
+            /// Fluent method for setting <see cref="ITopic.OnSuccess"/>/>.
+            /// </summary>
+            /// <param name="onSuccess">See <see cref="ITopic.OnSuccess"/>.</param>
+            /// <returns>A reference to <c>this</c> for fluent API calls.</returns>
             public TopicFluentInterface OnSuccess(Action<IBotContext> onSuccess)
             {
                 _topic._onSuccess = onSuccess;
                 return this;
             }
 
+            /// <summary>
+            /// Fluent method for setting <see cref="ITopic.OnFailure"/>/>.
+            /// </summary>
+            /// <param name="onFailure">See <see cref="ITopic.OnFailure"/>.</param>
+            /// <returns>A reference to <c>this</c> for fluent API calls.</returns>
             public TopicFluentInterface OnFailure(Action<IBotContext, string> onFailure)
             {
                 _topic._onFailure = onFailure;
@@ -117,24 +134,50 @@ namespace PromptlyBot
         }
     }
 
+    /// <summary>
+    /// Abstract class that represents a conversation topic that returns a value when it completes successfully.
+    /// </summary>
+    /// <typeparam name="TState">
+    /// The state of the conversation topic that will be persisted between conversation turns.
+    /// </typeparam>
+    /// <typeparam name="TValue">
+    /// The value that will be returned when the <c>Topic</c> completes successfully.
+    /// </typeparam>
     public abstract class Topic<TState, TValue> : Topic<TState> where TState : new()
     {
         private readonly TopicValueFluentInterface _set;
 
+        /// <summary>
+        /// See <see cref="Topic{TState}"/>.
+        /// </summary>
         public Topic() : base()
         {
             this._set = new TopicValueFluentInterface(this);
         }
 
+        /// <value>
+        /// See <see cref="Topic{TState}.Set"/>.
+        /// </value>
         new public TopicValueFluentInterface Set { get => _set; }
 
         private Action<IBotContext, TValue> _onSuccessValue;
+        /// <value>
+        /// Gets/Sets the delegate to be called when the conversation topic completes successfully.
+        /// </value>
+        /// <remarks>Note: For <see cref="Topic{TState, TValue}"/>, this delegate acccepts param of type <c>TValue</c> so the <c>Topic</c> can return a value when it completes successfully.</remarks>
         new public Action<IBotContext, TValue> OnSuccess { get => _onSuccessValue; set => _onSuccessValue = value; }
 
+        /// <summary>
+        /// Internal class to define the fluent API for <see cref="Topic{TState, TValue}"/>.
+        /// </summary>
         public class TopicValueFluentInterface
         {
             private readonly Topic<TState, TValue> _topic;
 
+            /// <summary>
+            /// Establishes reference between <see cref="Topic{TState, TValue}"/> and this fluent API class.
+            /// </summary>
+            /// <param name="topic">The <see cref="Topic{TState, TValue}"/> referenced in the fluent API.</param>
             public TopicValueFluentInterface(Topic<TState, TValue> topic)
             {
                 this._topic = topic;
