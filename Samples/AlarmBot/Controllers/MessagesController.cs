068b6eb138bb02f74882cb2c8249b4af621d6924
Chris Mayo <cmayo@microsoft.com>
2018-03-13 20:22:48 -0700

    Commented out unneeded controllers from M1.

--- Samples/AlarmBot/Controllers/MessagesController.cs
+++ Samples/AlarmBot/Controllers/MessagesController.cs
@@ -1,27 +1,27 @@
-﻿using AlarmBot.Topics;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Bot.Builder;
-using Microsoft.Bot.Builder.Adapters;
-using Microsoft.Bot.Schema;
-using System.Threading.Tasks;
+﻿//using AlarmBot.Topics;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.Bot.Builder;
+//using Microsoft.Bot.Builder.Adapters;
+//using Microsoft.Bot.Schema;
+//using System.Threading.Tasks;
 
-namespace AlarmBot.Controllers
-{
-    [Route("api/[controller]")]
-    public class MessagesController : BotController
-    {
-        public MessagesController(BotFrameworkAdapter adapter) : base(adapter) { }
+//namespace AlarmBot.Controllers
+//{
+//    [Route("api/[controller]")]
+//    public class MessagesController : BotController
+//    {
+//        public MessagesController(BotFrameworkAdapter adapter) : base(adapter) { }
 
-        protected override Task OnReceiveActivity(IBotContext context)
-        {
-            if (context.Request.Type == ActivityTypes.Message)
-            {
-                var rootTopic = new RootTopic(context);
+//        protected override Task OnReceiveActivity(IBotContext context)
+//        {
+//            if (context.Request.Type == ActivityTypes.Message)
+//            {
+//                var rootTopic = new RootTopic(context);
 
-                rootTopic.OnReceiveActivity(context);
-            }
+//                rootTopic.OnReceiveActivity(context);
+//            }
 
-            return Task.CompletedTask;
-        }
-    }
-}
+//            return Task.CompletedTask;
+//        }
+//    }
+//}
