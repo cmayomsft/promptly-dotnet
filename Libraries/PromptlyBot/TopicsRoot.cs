5d53a9b3a97fb9401b32c54ce59aaf7b22c07306
Chris Mayo <cmayo@microsoft.com>
2018-03-14 18:18:59 -0700

    First pass at M2 integration for library.

--- Libraries/PromptlyBot/TopicsRoot.cs
+++ Libraries/PromptlyBot/TopicsRoot.cs
@@ -3,11 +3,27 @@ using Microsoft.Bot.Builder.Core.Extensions;
 
 namespace PromptlyBot
 {
-    public abstract class TopicsRoot : ConversationTopic<ConversationTopicState>
+    public class TopicsRootState : StoreItem
+    {
+        public ConversationTopicState RootTopic { get; set; }
+    }
+    
+    public abstract class TopicsRoot<TConversationState> : ConversationTopic<ConversationTopicState> where TConversationState : TopicsRootState, new()
     {
         public TopicsRoot(IBotContext context) : base()
         {
-            this.State = context.GetConversationState<ConversationTopicState>();
+            if (context.GetConversationState<TConversationState>().RootTopic == null)
+            {
+                context.GetConversationState<TConversationState>().RootTopic = new ConversationTopicState();
+            }
+
+            this.State = context.GetConversationState<TConversationState>().RootTopic;
         }
     }
+    
+    // TopicsRoot needs to have the Type of ConversationState passed to it.
+    // ConversationState needs to have a property RootTopic of type ConversationState.
+
+    // TopicsRoot state must inherit from ConversationTopicState.
+    // context.GetConversationState<ConversationState> must be built using that state and have a property called RootTopic of type TopicsRootState
 }
