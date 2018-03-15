f0ad25ac84c1698ae9a02261081226b0a0868d90
Chris Mayo <cmayo@microsoft.com>
2018-03-14 18:56:21 -0700

    Removed unnecessary comments.

--- Libraries/PromptlyBot/TopicsRoot.cs
+++ Libraries/PromptlyBot/TopicsRoot.cs
@@ -20,10 +20,4 @@ namespace PromptlyBot
             this.State = context.GetConversationState<TConversationState>().RootTopic;
         }
     }
-    
-    // TopicsRoot needs to have the Type of ConversationState passed to it.
-    // ConversationState needs to have a property RootTopic of type ConversationState.
-
-    // TopicsRoot state must inherit from ConversationTopicState.
-    // context.GetConversationState<ConversationState> must be built using that state and have a property called RootTopic of type TopicsRootState
 }
