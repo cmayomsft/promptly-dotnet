f55389b4fcbde3a1245f897325577e1c48f3f30e
Chris Mayo <cmayo@microsoft.com>
2018-03-27 07:18:00 +0900

    First pass as demo.

--- Samples/SimplePrompt/Topics/RootTopic.cs
+++ Samples/SimplePrompt/Topics/RootTopic.cs
@@ -16,6 +16,30 @@ namespace SimplePrompt.Topics
     {
         public RootTopic(IBotContext context) : base(context)
         {
+
+        }
+
+        public override Task OnReceiveActivity(IBotContext context)
+        {
+            if (context.Request.Type == ActivityTypes.Message)
+            {
+                // Check to see if there is an active topic.
+
+
+                // If you don't have the state you need, prompt for it
+
+
+                // Now that you have the state you need (age and name), use it!
+                context.SendActivity($"Hello { this.State.Name }! You are { this.State.Age } years old.");
+                return Task.CompletedTask;
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
+
+            /*
             this.SubTopics.Add("namePrompt", (object[] args) =>
             {
                 var namePrompt = new TextPrompt();
@@ -67,12 +91,9 @@ namespace SimplePrompt.Topics
 
                 return agePrompt;
             });
-        }
+            */
 
-        public override Task OnReceiveActivity(IBotContext context)
-        {
-            if (context.Request.Type == ActivityTypes.Message)
-            {
+                /*
                 // Check to see if there is an active topic.
                 if (this.HasActiveTopic)
                 {
@@ -95,13 +116,6 @@ namespace SimplePrompt.Topics
                         .OnReceiveActivity(context);
                     return Task.CompletedTask;
                 }
+                */
 
-                // Now that you have the state you need (age and name), use it!
-                context.SendActivity($"Hello { this.State.Name }! You are { this.State.Age } years old.");
-                return Task.CompletedTask;
-            }
 
-            return Task.CompletedTask;
-        }
-    }
-}
