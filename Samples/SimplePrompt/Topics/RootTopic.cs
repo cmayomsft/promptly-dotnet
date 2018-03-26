f3c1efcce376669a6c54c4da7083883197a75bea
Chris Mayo <cmayo@microsoft.com>
2018-03-27 07:30:35 +0900

    Third pass at demo.

--- Samples/SimplePrompt/Topics/RootTopic.cs
+++ Samples/SimplePrompt/Topics/RootTopic.cs
@@ -16,6 +16,30 @@ namespace SimplePrompt.Topics
     {
         public RootTopic(IBotContext context) : base(context)
         {
+
+        }
+
+        public override Task OnReceiveActivity(IBotContext context)
+        {
+            if (context.Request.Type == ActivityTypes.Message)
+            {
+                // Check to see if there is an active topic.
+
+
+                // If you don't have the state you need, prompt for it
+
+
+                // Now that you have the state you need (age and name), use it!
+                context.SendActivity($"Hello { this.State.Name }! You are { this.State.Age } years old.");
+                return Task.CompletedTask;
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
+
+            /*
             this.SubTopics.Add("namePrompt", (object[] args) =>
                 {
                     var namePrompt = new TextPrompt();
@@ -45,97 +69,6 @@ namespace SimplePrompt.Topics
                         this.ClearActiveTopic();
 
                         this.State.Age = value;
-
-                        this.OnReceiveActivity(context);
-                    });
-
-                return agePrompt;
-            });
-
-        }
-
-        public override Task OnReceiveActivity(IBotContext context)
-        {
-            if (context.Request.Type == ActivityTypes.Message)
-            {
-                // Check to see if there is an active topic.
-                if (this.HasActiveTopic)
-                {
-                    this.ActiveTopic.OnReceiveActivity(context);
-                    return Task.CompletedTask;
-                }
-
-                // If you don't have the state you need, prompt for it
-                if (this.State.Name == null)
-                {
-                    this.SetActiveTopic("namePrompt")
-                        .OnReceiveActivity(context);
-                    return Task.CompletedTask;
-                }
-
-                if(this.State.Age == null)
-                {
-                    this.SetActiveTopic("agePrompt")
-                        .OnReceiveActivity(context);
-                    return Task.CompletedTask;
-                }
-
-                // Now that you have the state you need (age and name), use it!
-                context.SendActivity($"Hello { this.State.Name }! You are { this.State.Age } years old.");
-                return Task.CompletedTask;
-            }
-
-            return Task.CompletedTask;
-        }
-    }
-}
-
-            /*
-            this.SubTopics.Add("namePrompt", (object[] args) =>
-            {
-                var namePrompt = new TextPrompt();
-
-                namePrompt.Set
-                    .OnPrompt("What is your name?")
-                    .OnSuccess((ctx, value) =>
-                    {
-                        this.ClearActiveTopic();
-
-                        this.State.Name = value;
-
-                        this.OnReceiveActivity(context);
-                    })
-                    .OnFailure((ctx, reason) =>
-                    {
-                        this.ClearActiveTopic();
-
-                        context.SendActivity("I'm sorry I'm having issues understanding you.");
-
-                        this.OnReceiveActivity(context);
-                    });
-
-                return namePrompt;
-            });
-
-            this.SubTopics.Add("agePrompt", (object[] args) =>
-            {
-                var agePrompt = new IntPrompt();
-
-                agePrompt.Set
-                    .OnPrompt("How old are you?")
-                    .OnSuccess((ctx, value) =>
-                    {
-                        this.ClearActiveTopic();
-
-                        this.State.Age = value;
-
-                        this.OnReceiveActivity(context);
-                    })
-                    .OnFailure((ctx, reason) =>
-                    {
-                        this.ClearActiveTopic();
-
-                        context.SendActivity("I'm sorry I'm having issues understanding you.");
 
                         this.OnReceiveActivity(context);
                     });
